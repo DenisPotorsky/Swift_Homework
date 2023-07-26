48e3b16a988b167505438cf38440ce3b44652ffe
Den <denis-s2@yandex.ru>
2023-07-27 02:59:41 +0600

    первая задачка всё

--- Sem3_Task1.playground/Contents.swift
+++ Sem3_Task1.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
+/*
+ Есть словарь с видами чая и их стоимостью. Есть очередь людей, которые хотят заказать чай (можно представить её в виде массива видов чая, которые хотят заказать).
+ Необходимо последовательно выводить в консоль сколько заплатит покупатель (необходимо вывести его номер по порядку, чай, который он заказал, и стоимость).
+ */
+ 
+var tea: [String: Int] = ["Chempion": 230, "Tess": 280, "Piala": 250, "Basilur": 300]
+var persons: [String] = ["Chempion", "Tess", "Chempion", "Tess", "Chempion", "Piala", "Piala", "Basilur", "Basilur", "Basilur"]
 
-var greeting = "Hello, playground"
+for (index, element) in persons.enumerated(){
+    print(index + 1, element, tea[element]!)
+}
