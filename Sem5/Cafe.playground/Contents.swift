35a00a334af27462828b1716a62aabaf5d0522e0
DenisPotorsky <96338996+DenisPotorsky@users.noreply.github.com>
2023-08-05 14:35:19 +0600

    выполнил дз

--- Sem5/Cafe.playground/Contents.swift
+++ Sem5/Cafe.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-'''
+/*
 Создать протокол для меню. Протокол должен содержать стоимость и наименование.
 Стоимость и название должны быть get-only.
 Создайте класс кафе, в нём должен быть массив с наименованиями меню и инициализатор.
@@ -16,22 +16,54 @@ add помимо того, что выполняла всё тоже самое,
 Создать класс автомата с едой, не реализовывать в нём ни методов, ни свойств,
 однако, этот класс должен уметь делать всё тоже самое, что и FirstCafe.
 Сделать так, чтобы от класса автомата с напитками нельзя было наследоваться.
-'''
+*/
 
 protocol Menu {
-    prived var price: Double {get}
-    prived var nave: String {get}
+    var cost: Double {get}
+    var name: String {get}
 }
 
-class Cafe{
-    private var menuNames: [Menu]
-    init(menu: Menu) {
+class Cafe {
+    private var menu: [Menu]
+
+    init(menu: [Menu]){
         self.menu = menu
     }
-struct Tea: Menu {
-    enum TeaType {
-    case black, green, fruit
+    
+    func add(_ newPosition: Menu){
+        menu.append(newPosition)
+    }
+}
+
+struct Tea : Menu{
+    enum TeaType : String{
+        case green
+        case black
+        case fruit
+        case herbal
     }
     var type: TeaType
-    }
+    var name: String {type.rawValue + "tea"}
+    var cost: Double
+   init(type: TeaType, cost: Double) {
+    self.type = type
+    self.cost = cost
+   }
+}
 
+class FirstCafe : Cafe {
+    override func add(_ newPosition:Menu){
+        super.add(newPosition)
+        print(newPosition.cost)
+    }
+}
+
+class SecondCafe:Cafe{
+
+}
+
+class ThirdCafe: Cafe {
+
+}
+
+final class Vending : FirstCafe{}
