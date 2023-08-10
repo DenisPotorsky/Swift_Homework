d054943b44db2f85a90a3b5f231bb19c6fb61675
DenisPotorsky <96338996+DenisPotorsky@users.noreply.github.com>
2023-08-10 20:26:03 +0500

    новая папка для домашки, старая грузится не корректно

---
+++ App_Development/Sem_1/Project_2/Project_2/GroupsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GroupsViewController.swift
+//  Project_2
+//
+//  Created by Den on 10.08.2023.
+//
+
+import UIKit
+
+class GroupsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Groups"
+        view.backgroundColor = .white
+        tableView.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.barTintColor = .white
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        5
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        CustomTableViewCell2()
+    }
+}
